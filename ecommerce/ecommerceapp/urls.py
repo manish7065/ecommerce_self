fe09f4c4d9242a45b1d27246fbcd2806ff87b5da
manish <manumi296@gmail.com>
2024-10-23 13:23:14 +0530

    initial navbar mof=difications

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from ecommerceapp import views
 
 urlpatterns = [
-    path('', views.index, name="views"),
+    path('', views.index, name="index"),
+    path('index.html', views.index, name="index"),
     path('contact', views.contact, name="contact"),
     path('about', views.about, name='about')
 
