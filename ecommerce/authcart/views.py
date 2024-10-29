ba50fbad2a26ab91e6149d055f4b19f50462867d
manish <manumi296@gmail.com>
2024-10-30 03:16:10 +0530

    manu commit with auth

---
+++ ecommerce/authcart/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def signup(request):
+    return render(request,"authentication/signup.html")
+
+def handlelogin(request):
+    """
+    Logs the user into their account.
+
+    This view takes a request object as an argument and renders the login.html template.
+    """
+    return render(request,"authentication/login.html") 
+
+def handlelogout(request):
+    """
+    Logs the user out of their account.
+
+    This view takes a request object as an argument and renders the logout.html template.
+    """
+    return redirect('/auth/login')   
